0612c3756bf0720fed3ec9be5a11489e5520d2ba
PedroLavin <63043948+PedroLavin@users.noreply.github.com>
2022-09-11 18:25:59 +0200

    Update class.sh

--- class.sh
+++ class.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=60G
 
 # module load Python
-#module --ignore-cache load "Python/3.8"
-module load Python/3.9
+module --ignore-cache load "Python/3.8"
+#module load Python/3.9
 
-python3.9 ./Malware.py
+python3.8 Malware.py

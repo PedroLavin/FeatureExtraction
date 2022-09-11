4af2f8781487caa7be4a9053131bc88d3dd03338
PedroLavin <63043948+PedroLavin@users.noreply.github.com>
2022-09-11 18:13:26 +0200

    Update class.sh

--- class.sh
+++ class.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-#SBATCH --time=2:00:00
-#SBATCH --partition=cpufast
-#SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=1
-#SBATCH --mem=60G
+!/bin/bash
+SBATCH --time=2:00:00
+SBATCH --partition=cpufast
+SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=1
+SBATCH --mem=60G
 
 # module load Python
-# module --ignore-cache load "Python/3.8.2"
-module load Python/3.9
+# module --ignore-cache load "Python/3.8"
+#module load Python/3.9
 
 python ./Malware.py

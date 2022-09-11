1acc684a0d081c53a805d768f59e0814b606cb1e
PedroLavin <63043948+PedroLavin@users.noreply.github.com>
2022-09-11 18:14:21 +0200

    Update class.sh

--- class.sh
+++ class.sh
@@ -1,8 +1,8 @@
-!/bin/bash
-SBATCH --time=2:00:00
-SBATCH --partition=cpufast
-SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=1
-SBATCH --mem=60G
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --partition=cpufast
+#SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=1
+#SBATCH --mem=60G
 
 # module load Python
 # module --ignore-cache load "Python/3.8"
